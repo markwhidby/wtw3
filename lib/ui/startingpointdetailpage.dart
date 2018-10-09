f8d0146169711f4c6958595d0578b38ec2335a25
markwhidby <markwhidby@gmail.com>
2018-10-08 17:57:24 -0700

    Added Firebase plugins

--- lib/ui/startingpointdetailpage.dart
+++ lib/ui/startingpointdetailpage.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 
 import '../strings.dart';
 import '../models/startingpoint.dart';
